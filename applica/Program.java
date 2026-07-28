949f555017d38d06154428c65c9dc4b0a15af218
Eros Henrique <vasconcelos.eros@hotmail.com>
2026-07-28 20:26:19 -0300

    Moving pieces

--- applica/Program.java
+++ applica/Program.java
@@ -1,14 +1,32 @@
 package applica;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPosition;
+import chess.ChessPiece;
 
 
 public class Program {
 
     public static void main (String[] args) {
         
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessposition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessposition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+        
     }
     
 }
--- applica/UI.java
+++ applica/UI.java
@@ -1,7 +1,12 @@
 package applica;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
 
 public class UI {
 
@@ -25,6 +30,19 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUD = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUD = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUD = "\u001B[47m";
+
+    public static ChessPosition readChessposition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         
